e23f1782db635f535371a27b3e6b5bf5aae3f415
Luis Guisado <lguisadom@gmail.com>
2022-03-01 11:16:53 -0500

    Start

---
+++ src/main/java/com/nttdata/lagm/credit/SpringWebfluxCreditApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxCreditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/lagm/credit/SpringWebfluxCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
